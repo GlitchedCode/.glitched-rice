0cb397885027569ccf00d338b3b7cdd65b914ae3
Giuseppe La Malfa <peppe@glitchedcode.com>
2021-10-09 01:04:37 +0200

    updated theme install scripts, added feh to desktop apps

--- scripts/arch/desktop_apps.sh
+++ scripts/arch/desktop_apps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPO_PACKAGES="yay vim code i3-gaps conky-lua-nv cmus ranger neofetch lolcat tmux ttf-font-awesome maim pactl kitty rofi picom networkmanager blueman spectacle"
+REPO_PACKAGES="yay vim code i3-gaps conky-lua-nv cmus ranger neofetch lolcat tmux ttf-font-awesome maim playerctl kitty rofi picom networkmanager blueman spectacle nautilus feh"
 AUR_PACKAGES="polybar-git"
 
 sudo pacman -S ${PACKAGES}
