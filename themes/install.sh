85b995dd746c4461510333da414ffea9e0eaf7eb
Giuseppe La Malfa <peppe@glitchedcode.com>
2021-10-08 18:56:08 +0200

    fixed theme install, added picom.conf

--- themes/install.sh
+++ themes/install.sh
@@ -1,25 +1,29 @@
-#!/bin/bash -x
+#!/bin/bash
 
-if [[ -d ~/.glitched-rice/themes/$1 ]]; then
-	mkdir -p ~/.config/i3
-	mkdir -p ~/.glitched-rice/current
-	rm -rf ~/.glitched-rice/current/*
-	mkdir -p ~/.themes
+if [[ -d ~/.glitched-rice/themes/$1/ ]] && [[ -n $1  ]]; then
+	mkdir -p "$HOME/.config/i3"
+	mkdir -p "$HOME/.glitched-rice/current"
+	rm -rf "$HOME/.glitched-rice/current/*"
+	mkdir -p "$HOME/.themes"
 
 	# replacing i3 config
-	mv -f ~/.config/i3/config.old.old ~/.config/i3/config.old.old.old
-	mv -f ~/.config/i3/config.old ~/.config/i3/config.old.old
-	mv -f ~/.config/i3/config ~/.config/i3/config.old
-	cat ~/.glitched-rice/i3/config ~/.glitched-rice/themes/$1/i3theme > ~/.config/i3/config
+	mv -f "$HOME/.config/i3/config.old.old" "$HOME/.config/i3/config.old.old.old"
+	mv -f "$HOME/.config/i3/config.old" "$HOME/.config/i3/config.old.old"
+	mv -f "$HOME/.config/i3/config" "$HOME/.config/i3/config.old"
+	cat "$HOME/.glitched-rice/i3/config" "$HOME/.glitched-rice/themes/$1/i3theme" > "$HOME/.config/i3/config"
 	
-	ln -s ~/.glitched-rice/themes/$1/polybar-conf ~/.glitched-rice/current/
-	ln -s ~/.glitched-rice/themes/$1/rofi.rasi ~/.glitched-rice/current/
-	ln -s ~/.glitched-rice/themes/$1/conky-system ~/.glitched-rice/current/
-	ln -s ~/.glitched-rice/themes/$1/bg ~/.glitched-rice/current/
+	ln -s "$HOME/.glitched-rice/themes/$1/polybar-conf" "$HOME/.glitched-rice/current/"
+	ln -s "$HOME/.glitched-rice/themes/$1/rofi.rasi" "$HOME/.glitched-rice/current/"
+	ln -s "$HOME/.glitched-rice/themes/$1/conky-system" "$HOME/.glitched-rice/current/"
+	ln -s "$HOME/.glitched-rice/themes/$1/bg" "$HOME/.glitched-rice/current/"
 	
-	THEME_NAME_SCRIPT=~/.glitched-rice/themes/$1/theme-name.sh
-	cp -r "~/.glitched-rice/themes/$1/`${THEME_NAME_SCRIPT}`" ~/.themes/
+	THEME_NAME_SCRIPT="$HOME/.glitched-rice/themes/$1/theme-name.sh"
+	cp -r "$HOME/.glitched-rice/themes/$1/`${THEME_NAME_SCRIPT}`" "$HOME/.themes/"
 else
 	echo Invalid theme name. Available themes are:
-	ls -d ~/.glitched-rice/themes/*/
+	for f in "$HOME/.glitched-rice/themes/*"; do
+		if [[ -d $f ]]; then
+			basename $f
+		fi
+	done
 fi
