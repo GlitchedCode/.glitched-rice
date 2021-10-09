650ecbc5c2d3dbaab7208c38e3ff31c37704b095
Giuseppe La Malfa <peppe@glitchedcode.com>
2021-10-09 02:35:23 +0200

    fixed merge mess

--- scripts/gentoo/desktop_apps.sh
+++ scripts/gentoo/desktop_apps.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 
-<<<<<<< HEAD
 PACKAGES="vim vscode i3-gaps conky cmus ranger neofetch lolcat tmux fontawesome maim playerctl kitty rofi picom networkmanager blueman spectacle nautilus polybar feh"
-=======
-PACKAGES="vim vscode i3-gaps conky cmus ranger neofetch lolcat tmux fontawesome maim playerctl kitty rofi picom networkmanager blueman spectacle polybar feh"
->>>>>>> 49e24193d6c7beb5d9bc29f36f3031fb6fe074f2
 
 sudo emerge -a ${PACKAGES}
